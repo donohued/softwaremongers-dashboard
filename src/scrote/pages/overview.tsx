2d3bedf2376dd5de01ecb49aaf527fc10982f9d9
donohued <43716020+donohued@users.noreply.github.com>
2025-01-30 00:41:01 -0500

    add srsnix dash, themes, some features for srsnix

--- src/scrote/pages/overview.tsx
+++ src/scrote/pages/overview.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import '../scrote-style.css'
+//import '../scrote-style.css'
 
 export default function ScroteOverview() {
     const navigate = useNavigate();
---
+++ src/srsnix/srsnix-nav.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function SeriouslyNav() {
+    return (
+        <div style={{ width: '50%', margin: '12px auto', justifyContent: 'center', textAlign: 'center', color: 'white' }}>
+
+            <p style={{ fontSize: '1.25rem' }}>SeriouslyNIX.com</p>
+            <div><Link to={"/srsnix/overview"}>Overview</Link></div>
+            <div><Link to={"/srsnix/updates"}>Write Update</Link></div>
+            <div><Link to={"/srsnix/files"}>Manage Files</Link></div>
+            <div><Link to={"/srsnix/polls"}>Poll Manager</Link></div>
+            <div><Link to={"/srsnix/harold"}>Harold Manager</Link></div>
+            <div><Link to={"/srsnix/ip"}>Guest Manager</Link></div>
+            <div><Link to={"/srsnix/stats"}>StatTrak™</Link></div>
+
+        </div>
+    )
+}
